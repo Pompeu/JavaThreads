e6035f720eb41daf8a2535a1ae525c628a0ede29
mnmubeen <fam.nazar@gmail.com>
2017-04-25 13:38:19 +0530

    Changed code to readable format and added more codes for better understandings.

--- src/Threads01/demo3/app.java
+++ src/Threads01/demo3/app.java
@@ -3,11 +3,12 @@ package Threads01.demo3;
 public class app {
 	public static void main(String[] args) {
 		Thread t1 = new Thread(new Runnable() {
-
+		
 			@Override
 			public void run() {
+			Thread.currentThread().setName("only thread");
 				for (int i = 0; i < 10; i++) {
-					System.out.println("Ola !!!!" + i);
+					System.out.println("Looping !!!!"+  Thread.currentThread().getName() +" with value "+ i);
 					try {
 						Thread.sleep(100);
 					} catch (InterruptedException e) {
